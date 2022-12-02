b7854be6dc40324780594af38cfc23b95ecd8890
Dave Patric <dkpatric@aol.com>
2022-12-02 18:41:53 -0500

    Day01 completed

--- Day01/day01.py
+++ Day01/day01.py
@@ -15,12 +15,20 @@ def load_file(filename):
 
 def main():
     filename = 'Day01/input.txt'
+    # filename = 'Day01/test.txt'
     elf_list = load_file(filename)
     max_elf = elf_list[0]
     for i in range(1, len(elf_list)):
         if elf_list[i] > max_elf:
             max_elf = elf_list[i]
-    print('Elf with most calories is carrying:', max_elf )    
+    print('Part 1: Elf with most calories is carrying', max_elf, 'calories')  
+
+    # Part 2
+    elf_list.sort(reverse=True)
+    top_three = 0
+    for i in range(3):
+        top_three += elf_list[i]
+    print('Part 2: Top 3 elves are carrying', top_three, 'calories')
 
 if __name__ == '__main__':
     main()
